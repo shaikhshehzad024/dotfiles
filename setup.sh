e49d45fc0acc2a8268b145f2139e02449209e10a
Shehzad Shaikh <81687930+shaikhshehzad024@users.noreply.github.com>
2025-04-28 16:54:31 +0530

    added bashrc again to set zsh as defualt

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ pkg install x11-repo
 pkg install tigervnc fluxbox clang nodejs python wget zsh curl fzf git neovim
 chmod +x $HOME/.vnc/xstartup $HOME/scripts/*
 
+chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
